0e16bfdd18723954ac7023306b78abb9d5c607f9
Agnogh <agnogh_orion@yahoo.com>
2025-04-22 01:22:18 +0200

    correcting font size and function planning

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,3 +1,10 @@
-function user_selected_number() {
-    text.html
+/* function user_selected_number() {
+    let user_number = 
+    
 }
+
+function compare_values_between_user_selection_and_random_number() {
+    if (user_selected_number) === radnom_created_number();
+    win++
+}
+    */
