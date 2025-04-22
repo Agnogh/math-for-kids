88a6d4e5e7ffba5c72c9223ff6aa8abf61161a24
Agnogh <agnogh_orion@yahoo.com>
2025-04-22 18:46:43 +0200

    trzing to add fucntions to buttons

--- assests/js/script.js
+++ assests/js/script.js
@@ -3,7 +3,7 @@ const redButton = document.getElementsByClassName("btn_1");
 
 redButton.AddEventListener("mousedown", clickedTheButton);
     function clickedTheButton(e) {
-    e.currentTarget.style.backgroundColor = "#e84610";
+    e.currentTarget.style.backgroundColor = "red";
     }
 
 
@@ -12,12 +12,40 @@ redButton.AddEventListener("mousedown", clickedTheButton);
 /* 
             <button class="btn_1" onclick="user_selected_number()">1</button>
 */
+
+/*
 const btn_1 = document.getElementsByClassName("btn_1");
 btn_1.AddEventListener("click", function(e) {
     const name = prompt("What is your name?");
-
+    alert("test")
     document.body.innerHTML = "<h1>Welcome, " + name + "</h1>";
 });
+*/
+
+
+/* or we do it like this */
+const btn_1 = document.getElementById("btn_1")
+btn_1.AddEventListener("click", testingTheFunction)
+
+function testingTheFunction() {
+    alert("hello there")
+}
+
+const randomNumber = document.getElementById("random_number_button")
+randomNumber.AddEventListener("click", function(p) {
+    if (user_entry === randomEntry) {
+        print (`Congrats! You selected ${user_entry} that matches ${randomEntry}`)
+        ++win
+    } else {
+        print (`You selected ${user_entry}, while number drawn was ${randomEntry}`)
+        ++lose
+    }
+})
+
+
+let win = 0;
+let lose = 0;
+
 
 /* 
 function user_selected_number() {
