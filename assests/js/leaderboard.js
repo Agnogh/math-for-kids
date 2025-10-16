24f4640625b745e02df1317455402c4dfa396c8d
Agnogh <agnogh_orion@yahoo.com>
2025-10-16 21:16:12 +0200

    adding js file for leaderboard to make things clean + wrapper

---
+++ assests/js/leaderboard.js
@@ -0,0 +1,9 @@
+
+(function () {
+  const KEY = 'guessGame.scores';
+  const table = document.getElementById('board');
+  const tbody = document.getElementById('leaderboard_rows');
+  const empty = document.getElementById('empty_state');
+  const clearBtn = document.getElementById('clear_btn');
+  const note = document.getElementById('last_saved_note');
+})
